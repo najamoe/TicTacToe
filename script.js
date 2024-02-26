53fd5d3d228b49323ab41c75b4408e80c8a09693
Naja <113134845+najamoe@users.noreply.github.com>
2024-02-26 14:05:38 +0000

    3rd exercise complete

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+window.addEventListener("load", start);
+
+function start (){
+    makeBoardClickable();
+}
+
+//-----------VIEW-----------
+function selectCell(row, col){
+    writeToCell(row, col, 1);
+    displayBoard();
+}
+
+function makeBoardClickable(){
+    document.querySelector('#board')
+    .addEventListener("click", boardClicked)
+}
+
+
+
+function boardClicked(event) {
+   const cell= event.target;
+   const row = cell.dataset.row;
+   const col = cell.dataset.col;
+
+   console.log(`clicked on row: ${row} col: ${col}`);
+   selectCell(row, col);
+}
+
+function displayBoard(){
+    for (let row = 0; row < 3; row++){
+        for (let col = 0; col < 3; col ++){
+            const value = readFromCell(row, col);
+            const cell = document
+            .querySelector(`[data-row="${row}"][data-col="${col}"]`);
+
+
+            switch(value) {
+                case 0: cell.textContent = " "; break;
+                case 1: cell.textContent = "X"; break;
+                case 2: cell.textContent = "O"; break;
+            }
+        }
+    }
+}
+
+//------------MODEL--------------
+
+const model = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
+function writeToCell(row, col, value){
+    model[row][col] = value;
+}
+
+function readFromCell(row, col) {
+    return model[row][col];
+}
+
+
